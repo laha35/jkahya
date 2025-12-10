ae56325ebdc26b721283db5522d2e8f2c28c6e1a
JKI <xjdius37@gmail.com>
2025-12-10 12:37:59 +0900

    feat: add service worker for caching strategies and update layout with PWA icons and manifest

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public", // 서비스 워커 파일이 생성될 위치
+  cacheOnFrontEndNav: true, // 프론트엔드 네비게이션 시 캐싱 활성화
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true, // 온라인 상태가 되면 새로고침
+  swcMinify: true,
+  //disable: process.env.NODE_ENV === "development", // 개발 모드에서는 PWA 비활성화 (권장)
+  // workboxOptions: { ... } // 필요 시 워크박스 상세 설정
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
